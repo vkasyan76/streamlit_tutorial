226cad1577e72afdc433b968f7d6df3ca677ea33
vkasyan76 <vkasyan@gmx.de>
2024-12-06 11:50:28 +0100

    layouts

--- layouts.py
+++ layouts.py
@@ -15,3 +15,54 @@ with tab2:
     st.write("You are in tab 2")
 with tab3: 
     st.write("You are in tab 3")
+    
+# Columns layout
+col1, col2 = st.columns(2)
+with col1:
+    st.header("Column 1")
+    st.write("Content for column 1")
+with col2:
+    st.header("Column 2")
+    st.write("Content for column 2")
+    
+# col1, col2, col3 = st.columns(3)
+
+# with col1:
+#     st.header("A cat")
+#     st.image("https://static.streamlit.io/examples/cat.jpg")
+
+# with col2:
+#     st.header("A dog")
+#     st.image("https://static.streamlit.io/examples/dog.jpg")
+
+# with col3:
+#     st.header("An owl")
+#     st.image("https://static.streamlit.io/examples/owl.jpg")
+
+# Containers example
+with st.container(border=True):
+    st.write("This is inside a container")
+    st.write("You can think of containers as a grouping for elements")
+    st.write("Containers help manage sections of the page")
+
+# Empty placeholder
+placeholder = st.empty()
+placeholder.write("This is an empty placeholder, usefull for dynamic content.")
+
+if st.button("Update Placeholder"):
+    placeholder.write("The content of this placeholder has been updated.")
+    
+# Expander
+with st.expander("Expand for more details"):
+    st.write("This is additional information that is hidden by default.")
+    st.write("You can use expanders to keep your interface cleaner")
+
+# Popover (Tooltip)
+st.write("Hover over this button for a tooltip")
+st.button("Button with Tooltip", help="This is a tooltip or popover or hover.")
+
+#Sidebar input handling
+if sidebar_input:
+    st.write(f"You entered in the sidebar: {sidebar_input}")
+
+    
